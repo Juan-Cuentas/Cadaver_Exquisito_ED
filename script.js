d8b2ab16e3e4750be81d0b15a44fe56b9a979506
Juan-Cuentas <juana.cuentasb@utadeo.edu.co>
2026-03-26 02:14:20 +0000

    cambio nombre carpetas

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function irRandom() {
   const paginas = [
-    "parte 1/cap1_part1.html",
-    "parte 1/cap2_part1.html",
-    "parte 1/cap3_part1.html",
-    "parte 1/cap4_part1.html"
+    "parte_1/cap1_part1.html",
+    "parte_1/cap2_part1.html",
+    "parte_1/cap3_part1.html",
+    "parte_1/cap4_part1.html"
   ];
 
   const random = Math.floor(Math.random() * paginas.length);
