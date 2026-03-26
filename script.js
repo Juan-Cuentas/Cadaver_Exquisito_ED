ae611f307ed257e13e183dca0d158aa09a4563f5
Juan-Cuentas <juana.cuentasb@utadeo.edu.co>
2026-03-26 02:24:15 +0000

    Eliminación carpeta pa ver si funciona

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function irRandom() {
   const paginas = [
-    "parte_1/cap1_part1.html",
-    "parte_1/cap2_part1.html",
-    "parte_1/cap3_part1.html",
-    "parte_1/cap4_part1.html"
+    "cap1_part1.html",
+    "cap2_part1.html",
+    "cap3_part1.html",
+    "cap4_part1.html"
   ];
 
   const random = Math.floor(Math.random() * paginas.length);
