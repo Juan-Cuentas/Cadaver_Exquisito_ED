6e4b367fbabf4097f0729a4bee3a1013de22fd26
Juan-Cuentas <juana.cuentasb@utadeo.edu.co>
2026-03-26 02:31:42 +0000

    cap 5 entra

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function irRandom() {
     "cap1_part1.html",
     "cap2_part1.html",
     "cap3_part1.html",
-    "cap4_part1.html"
+    "cap4_part1.html",
+    "cap5_part1.html"
   ];
 
   const random = Math.floor(Math.random() * paginas.length);
